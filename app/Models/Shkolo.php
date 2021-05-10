1fe2b59b9719700d487b6a752c785639bada3eb5
Dimi Kiro <mitak.1985@abv.bg>
2021-05-10 18:32:15 +0300

    added Procfile

--- app/Models/Shkolo.php
+++ app/Models/Shkolo.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Shkolo extends Model
 {
     use HasFactory;
-     protected $fillable = ['title', 'link', 'color'];
+     protected $fillable = ['title', 'link', 'color']; /*Всички модели Laravel, когато са създадени, не съдържат свойство за попълване.
+За да им позволим да приемат полеви данни и да ги вмъкнат в базата данни, първо трябва да ги разрешим в нашия модел.*/
 }
